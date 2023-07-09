2b9dbd52ad2106b519e2605da9bea911f55a4d58
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2023-07-09 18:03:05 +0900

    [Gold I] Title: 트리의 독립집합, Time: 504 ms, Memory: 35464 KB -BaekjoonHub

--- 백준/Gold/2213. 트리의 독립집합/트리의 독립집합.py
+++ 백준/Gold/2213. 트리의 독립집합/트리의 독립집합.py
@@ -1,21 +1,30 @@
 import copy
 
 def find_set(i):
+    global visited
     hap1 = 0
     lst1 = []
     hap2 = w_lst[i-1]
     lst2 = [i]
     for i1 in t_connect[i]:
-        a, b= find_set(i1)
+        if visited[i1]:
+            a, b = visited[i1]
+        else:
+            a, b = find_set(i1)
         hap1 += a
         lst1 += b
         for i2 in t_connect[i1]:
-            a, b = find_set(i2)
+            if visited[i2]:
+                a, b = visited[i2]
+            else:
+                a, b = find_set(i2)
             hap2 += a
             lst2 += b
     if hap1 > hap2:
+        visited[i] = [hap1, lst1]
         return hap1, lst1
     else:
+        visited[i] = [hap2, lst2]
         return hap2, lst2
 
 n = int(input())
@@ -41,6 +50,7 @@ while stack:
                 t_connect[i].append(j)
                 n_stack.append(j)
     stack = copy.deepcopy(n_stack)
+visited = [False] * (n+1)
 a, b = find_set(1)
 b.sort()
 print(a)
