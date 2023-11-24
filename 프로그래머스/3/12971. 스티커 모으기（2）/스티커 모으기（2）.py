4e4023be53929e7d4e3d02ffd7a8c2f46da09f28
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2023-11-24 10:18:58 +0900

    [level 3] Title: 스티커 모으기(2), Time: 70.59 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -0,0 +1,11 @@
+def solution(sticker):
+    answer = [[0, 0], [0, sticker[0]]]
+    # 첫 집을 털었는지, 이전 집을 털었는지...
+    for i in range(1, len(sticker) - 1):
+        new = [[0, 0], [0, 0]]
+        new[0][0] = max(answer[0])
+        new[0][1] = answer[0][0] + sticker[i]
+        new[1][0] = max(answer[1])
+        new[1][1] = answer[1][0] + sticker[i]
+        answer = new
+    return max(answer[0][0] + sticker[-1], answer[0][1], max(answer[1]))
