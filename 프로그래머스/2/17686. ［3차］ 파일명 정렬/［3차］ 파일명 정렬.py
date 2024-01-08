49f70a206d750bf7388cd564a43104670abe250f
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2024-01-08 15:58:59 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 1.77 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,21 +1,3 @@
 def solution(files):
     answer = []
-    files_sort = []
-    for file in files:
-        head = ''
-        number = ''
-        in_dec = False
-        for c in file:
-            if c.isdecimal():
-                in_dec = True
-                number += c
-                continue
-            else:
-                if in_dec:
-                    break
-                head += c
-        files_sort. append([head.lower(), int(number), file])
-    files_sort.sort(key=lambda x : x[:2])
-    for _, _, file in files_sort:
-        answer.append(file)
     return answer
