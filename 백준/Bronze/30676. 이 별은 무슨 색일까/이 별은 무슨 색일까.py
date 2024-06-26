e1c5a3ea249c50f0cff337accd3d85c98ead0e56
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2024-06-26 21:48:38 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,15 @@
+lamb = int(input())
+if lamb < 425:
+    print('Violet')
+elif lamb < 450:
+    print('Indigo')
+elif lamb < 495:
+    print('Blue')
+elif lamb < 570:
+    print('Green')
+elif lamb < 590:
+    print('Yellow')
+elif lamb < 620:
+    print('Orange')
+else:
+    print('Red')
