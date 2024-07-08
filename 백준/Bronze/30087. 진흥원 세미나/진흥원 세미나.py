bf3e891f880b5591ae93ee8a11f9ec6bb056e7ef
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2024-07-08 17:53:33 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,10 @@
+N = int(input())
+semina = {'Algorithm' : '204',
+          'DataAnalysis' : '207',
+          'ArtificialIntelligence' : '302',
+          'CyberSecurity' : 'B101',
+          'Network' : '303',
+          'Startup' : '501',
+          'TestStrategy' : '105'}
+for _ in range(N):
+    print(semina[input()])
