5d74854c84d54ca2c70d1977676b4fcf40bb0889
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2024-07-09 16:58:24 +0900

    [Silver II] Title: 주차장, Time: 276 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/5464. 주차장/주차장.py
@@ -0,0 +1,37 @@
+N, M = map(int, input().split())
+R = []
+for _ in range(N):
+    R.append(int(input()))
+W = []
+for _ in range(M):
+    W.append(int(input()))
+full = [False] * N
+empty = N
+where = [False] * M
+que = []
+ans = 0
+for _ in range(2 * M):
+    i = int(input())
+    if i > 0:
+        i -= 1
+        if empty:
+            empty -= 1
+            for j in range(N):
+                if not full[j]:
+                    full[j] = True
+                    where[i] = j
+                    ans += R[j] * W[i]
+                    break
+        else:
+            que.append(i)
+    else:
+        i = -i - 1
+        if que:
+            j = where[i]
+            i = que.pop(0)
+            where[i] = j
+            ans += R[j] * W[i]
+        else:
+            empty += 1
+            full[where[i]] = False
+print(ans)
