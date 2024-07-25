e53e2f00607ec46db7d798f56603072d0bea69a8
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2024-07-25 14:01:29 +0900

    [level 3] Title: 합승 택시 요금, Time: 155.77 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,38 +1,27 @@
 def solution(n, s, a, b, fares):
-    S = [20000000] * (n+1)
-    A = [20000000] * (n+1)
-    B = [20000000] * (n+1)
+    
+    def routes(e):
+        E = [20000000] * (n+1)
+        stk = [e]
+        E[e] = 0
+        while stk:
+            now = stk.pop()
+            for way in ways[now]:
+                if E[now] + way[1] < E[way[0]]:
+                    E[way[0]] = E[now] + way[1]
+                    if way[0] not in stk:
+                        stk.append(way[0])
+        return E
+
     ways = [[] for _ in range(n+1)]
     for fare in fares:
         ways[fare[0]].append([fare[1], fare[2]])
         ways[fare[1]].append([fare[0], fare[2]])
-    stk = [s]
-    S[s] = 0
-    while stk:
-        now = stk.pop()
-        for way in ways[now]:
-            if S[now] + way[1] < S[way[0]]:
-                S[way[0]] = S[now] + way[1]
-                if way[0] not in stk:
-                    stk.append(way[0])
-    stk = [a]
-    A[a] = 0
-    while stk:
-        now = stk.pop()
-        for way in ways[now]:
-            if A[now] + way[1] < A[way[0]]:
-                A[way[0]] = A[now] + way[1]
-                if way[0] not in stk:
-                    stk.append(way[0])
-    stk = [b]
-    B[b] = 0
-    while stk:
-        now = stk.pop()
-        for way in ways[now]:
-            if B[now] + way[1] < B[way[0]]:
-                B[way[0]] = B[now] + way[1]
-                if way[0] not in stk:
-                    stk.append(way[0])
+    
+    S = routes(s)
+    A = routes(a)
+    B = routes(b)
+
     answer = 60000000
     for i in range(1, n+1):
         if S[i] + A[i] + B[i] < answer:
