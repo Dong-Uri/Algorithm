89084a9d7d1bc79713161c3419f38a0b1440c941
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2024-07-26 14:00:07 +0900

    [Gold III] Title: LCA, Time: 312 ms, Memory: 130068 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.py
+++ 백준/Gold/11437. LCA/LCA.py
@@ -7,7 +7,10 @@ for _ in range(N - 1):
     a, b = map(int, input().split())
     connect[a].append(b)
     connect[b].append(a)
-node_info = [[0, 0] for _ in range(N + 1)]
+logn = 0
+while 2 ** logn < N:
+    logn += 1
+node_info = [[0] * (logn + 1) for _ in range(N + 1)]
 node_info[1][0] = 1
 stack = [1]
 while stack:
@@ -16,16 +19,34 @@ while stack:
         if not node_info[n][0]:
             node_info[n][0] = node_info[node][0] + 1
             node_info[n][1] = node
+            for i in range(2, logn + 1):
+                nn = node_info[n][i - 1]
+                if not node_info[nn][i - 1]:
+                    break
+                node_info[n][i] = node_info[nn][i - 1]
             stack.append(n)
 
 M = int(input())
 for _ in range(M):
     a, b = map(int, input().split())
-    while node_info[a][0] > node_info[b][0]:
-        a = node_info[a][1]
-    while node_info[a][0] < node_info[b][0]:
-        b = node_info[b][1]
+    if node_info[a][0] > node_info[b][0]:
+        l = node_info[a][0] - node_info[b][0]
+        i = 1
+        while bin(l)[-i] != 'b':
+            if bin(l)[-i] == '1':
+                a = node_info[a][i]
+            i += 1
+    if node_info[a][0] < node_info[b][0]:
+        l = node_info[b][0] - node_info[a][0]
+        i = 1
+        while bin(l)[-i] != 'b':
+            if bin(l)[-i] == '1':
+                b = node_info[b][i]
+            i += 1
     while a != b:
-        a = node_info[a][1]
-        b = node_info[b][1]
+        for i in range(2, logn + 1):
+            if node_info[a][i] == node_info[b][i]:
+                a = node_info[a][i - 1]
+                b = node_info[b][i - 1]
+                break
     print(a)
