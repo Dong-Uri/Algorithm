acbbe6324f384bd796e1aa988e681e50028b35c4
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2024-08-16 17:40:43 +0900

    [Silver I] Title: 1, 2, 3 더하기 9, Time: 436 ms, Memory: 48676 KB -BaekjoonHub

---
+++ 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.py
@@ -0,0 +1,16 @@
+T = int(input())
+arr = [[0] * 1001 for _ in range(3)]
+arr[0][0] = 1
+arr[1][1] = 1
+arr[2][1] = 1
+arr[2][2] = 1
+i = 3
+while i <= 1000:
+    lst = [0] * 1001
+    for j in range(1, 1001):
+        lst[j] = (arr[-1][j - 1] + arr[-2][j - 1] + arr[-3][j - 1]) % 1000000009
+    arr.append(lst)
+    i += 1
+for _ in range(T):
+    n, m = map(int, input().split())
+    print(sum(arr[n][:m + 1]) % 1000000009)
