8d5ea17f6ae2596cc97e6bcd3206594868c02856
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2024-12-13 12:22:04 +0900

    Title: 코드트리 DB, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241213/코드트리 DB/codetree-db.py
+++ 241213/코드트리 DB/codetree-db.py
@@ -1,73 +1,44 @@
 import heapq
+from bisect import bisect_left, insort
 
 Q = int(input())
 for _ in range(Q):
-    q = list(input().split())
-
+    q = input().split()
+    
     if q[0] == 'init':
-        name_dict = dict()
-        value_dict = dict()
-        value_sort = [0, 1000000001]
-
-    if q[0] == 'insert':
+        name_dict = {}
+        value_dict = {}
+        value_sort = [0, 1000000001]  # 경계값 유지
+    
+    elif q[0] == 'insert':
         name = q[1]
         value = int(q[2])
-        if name in name_dict.keys() and name_dict[name]:
+        if name in name_dict or value in value_dict:
             print(0)
-            continue
-        if value in value_dict.keys() and value_dict[value]:
-            print(0)
-            continue
-        name_dict[name] = value
-        value_dict[value] = name
-        l = 0
-        r = len(value_sort) - 1
-        while l + 1 < r:
-            if value < value_sort[(l + r) // 2]:
-                r = (l + r) // 2
-            else:
-                l = (l + r) // 2
-                if value_sort[l] == value:
-                    r = l + 1
-        value_sort = value_sort[:l + 1] + [value] + value_sort[r:]
-        print(1)
+        else:
+            name_dict[name] = value
+            value_dict[value] = name
+            insort(value_sort, value)  # 정렬된 위치에 삽입
+            print(1)
     
-    if q[0] == 'delete':
+    elif q[0] == 'delete':
         name = q[1]
-        if name not in name_dict.keys() or not name_dict[name]:
+        if name not in name_dict:
             print(0)
-            continue
-        value = name_dict[name]
-        name_dict[name] = 0
-        value_dict[value] = 0
-        l = 0
-        r = len(value_sort) - 1
-        while l + 1 < r:
-            if value < value_sort[(l + r) // 2]:
-                r = (l + r) // 2
-            else:
-                l = (l + r) // 2
-                if value_sort[l] == value:
-                    r = l + 1
-        value_sort = value_sort[:l] + value_sort[r:]
-        print(value)
-
-    if q[0] == 'rank':
+        else:
+            value = name_dict.pop(name)
+            value_dict.pop(value)
+            value_sort.remove(value)  # 리스트에서 삭제
+            print(value)
+    
+    elif q[0] == 'rank':
         k = int(q[1])
-        if k > len(value_sort) - 2:
+        if k >= len(value_sort) - 1:  # 유효한 k 확인
             print('None')
-            continue
-        print(value_dict[value_sort[k]])
-
-    if q[0] == 'sum':
+        else:
+            print(value_dict[value_sort[k]])
+    
+    elif q[0] == 'sum':
         k = int(q[1])
-        l = 0
-        r = len(value_sort) - 1
-        while l + 1 < r:
-            if k < value_sort[(l + r) // 2]:
-                r = (l + r) // 2
-            else:
-                l = (l + r) // 2
-                if value_sort[l] == k:
-                    r = l + 1
-        print(sum(value_sort[:l + 1]))
+        idx = bisect_left(value_sort, k)  # k 이하의 값까지의 합 계산
+        print(sum(value_sort[:idx]))
