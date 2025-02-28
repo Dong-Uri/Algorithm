997f2c40d0cc92909ba23abff6ee670a1672b81a
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2025-02-28 20:49:28 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250228/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int x, y, a;
+    char b;
+    cin >> a >> b >> x >> b >> y;
+    cout << "010-" << y << "-" << x;
+    return 0;
+}
