68889025e89dad1c8141a621637778650b7f7986
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2025-02-28 23:58:11 +0900

    Title: 문자열 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250228/문자열 출력하기/print-string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    for (N ; N > 0 ; N--) cout << "LeebrosCode\n";
+    return 0;
+}
