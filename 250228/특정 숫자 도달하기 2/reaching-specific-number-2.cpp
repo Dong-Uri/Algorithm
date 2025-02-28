3db76a27bb178b179908857b381fddbee882f62e
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2025-02-28 23:41:48 +0900

    Title: 특정 숫자 도달하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250228/특정 숫자 도달하기 2/reaching-specific-number-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    for (N ; N >= 1 ; N--) cout << N << " ";
+    return 0;
+}
