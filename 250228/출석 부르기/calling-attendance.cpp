4e4725d106b7dbae3a97b21a295bf4dad2b7c854
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2025-02-28 22:15:13 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250228/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A;
+    cin >> A;
+    if (A == 1) cout << "John";
+    else if (A == 2) cout << "Tom";
+    else if (A == 3) cout << "Paul";
+    else cout << "Vacancy";
+    return 0;
+}
