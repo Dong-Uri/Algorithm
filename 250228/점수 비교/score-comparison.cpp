57b73c80cae8b95d7e34ede1ee862e6eb2f738ec
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2025-02-28 22:49:38 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250228/점수 비교/score-comparison.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A1, A2, B1, B2;
+    cin >> A1 >> A2 >> B1 >> B2;
+    cout << (A1 > B1 && A2 > B2);
+
+    return 0;
+}
