57021db0718e019b6f3994ea9c059dd609d10633
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2025-02-28 22:08:14 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250228/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int s;
+    cin >> s;
+    cout << (s == 100 ? "pass" : "failure");
+    return 0;
+}
