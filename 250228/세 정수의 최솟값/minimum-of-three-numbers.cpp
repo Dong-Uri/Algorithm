80007b622a79fd7de149bf623b5a36ca9363733b
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2025-02-28 22:55:31 +0900

    Title: 세 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250228/세 정수의 최솟값/minimum-of-three-numbers.cpp
+++ 250228/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -5,7 +5,7 @@ int main() {
     // Please write your code here.
     int a, b, c;
     cin >> a >> b >> c;
-    if (a <= b && b <= c) cout << a;
+    if (a <= b && a <= c) cout << a;
     else if (b <= c) cout << b;
     else cout << c;
     return 0;
