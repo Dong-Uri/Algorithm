c9999498ccf43f737d1931b58dbbb13ad5eadc58
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2025-02-28 23:09:08 +0900

    Title: 두 사람, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250228/두 사람/two-person.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    char aa, bb;
+    cin >> a >> aa >> b >> bb;
+    cout << (a >= 19 && aa == 'M' || b >= 19 && bb == 'M');
+    return 0;
+}
