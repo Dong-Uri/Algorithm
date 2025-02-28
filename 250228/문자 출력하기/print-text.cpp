e1240ea60195be36416528803856fd8fb63dca4e
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2025-02-28 23:57:08 +0900

    Title: 문자 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250228/문자 출력하기/print-text.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char G;
+    cin >> G;
+    for (int i = 8 ; i > 0 ; i--) cout << G;
+    return 0;
+}
