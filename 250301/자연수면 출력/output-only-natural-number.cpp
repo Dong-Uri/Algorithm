14a5f21f2466070703a37a49d34ea7e92f6f0e7d
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2025-03-01 00:09:01 +0900

    Title: 자연수면 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250301/자연수면 출력/output-only-natural-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+    if (A > 0) for (B ; B > 0 ; B--) cout << A;
+    else cout << 0;
+    return 0;
+}
