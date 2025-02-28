4cada36fbba03fc485663704f3f83bb6bd47cbd0
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2025-03-01 00:20:50 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250301/369 게임/369-game.cpp
+++ 250301/369 게임/369-game.cpp
@@ -6,7 +6,9 @@ int main() {
     int N;
     cin >> N;
     for (int i = 1 ; i <= N ; i++) {
-        if (i % 3 == 0 || i % 10 == 3 || (i / 10) % 10 == 3) cout << 0 << " ";
+        int a = i % 10;
+        int b = (i / 10) % 10;
+        if (i % 3 == 0 || a == 3 || a == 6 || a == 9 || b == 3 || b == 6 || b == 9) cout << 0 << " ";
         else cout << i << " ";
     }
     return 0;
