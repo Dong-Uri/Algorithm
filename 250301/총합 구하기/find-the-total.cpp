3b7d48e8e4c38c2389b49c01081c0124cdea2cf7
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2025-03-01 00:58:25 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250301/총합 구하기/find-the-total.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+    int s = 0;
+    for (A ; A <= B; A++){
+        if (A % 6 == 0 && A % 8) s += A;
+    }
+    cout << s;
+    return 0;
+}
