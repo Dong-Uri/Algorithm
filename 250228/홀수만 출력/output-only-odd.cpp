1fccea1f449ee79eb0700206c3eb8e41360a9f14
Dongwoo Lee <109489851+Dong-Uri@users.noreply.github.com>
2025-02-28 23:40:09 +0900

    Title: 홀수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250228/홀수만 출력/output-only-odd.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+    for (A ; A <= B ; A += 2) cout << A << " ";
+    return 0;
+}
